4b9a88db966f8bbbd21e416de66923cec31e6fed
fez <eugenefezeu@gmail.com>
2020-02-01 14:10:02 +0100

    salut le monde

---
+++ gui/public/precache-manifest.6db1baff5b274c1dc06912db57bdc852.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8f6eb099c431e53f5c7",
+    "url": "/css/app.f54e6a10.css"
+  },
+  {
+    "revision": "0fcdf1aadbdad87cb13a",
+    "url": "/css/chunk-vendors.5cb32715.css"
+  },
+  {
+    "revision": "bf99607c771a956ecc1c8ec8a0b065b8",
+    "url": "/ec5c8c5a49ff89c422e7.worker.js"
+  },
+  {
+    "revision": "9f6ade983374108b70a1fdc75666333e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1c187ffc44ae2fc3ef29",
+    "url": "/js/about.554f6c67.js"
+  },
+  {
+    "revision": "a8f6eb099c431e53f5c7",
+    "url": "/js/app.f5416ce1.js"
+  },
+  {
+    "revision": "0fcdf1aadbdad87cb13a",
+    "url": "/js/chunk-vendors.bbef5f40.js"
+  },
+  {
+    "revision": "5a485a9e0f75998a40edbdc62b23b775",
+    "url": "/manifest.json"
+  }
+]);
